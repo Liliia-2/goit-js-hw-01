995a77a770ba3578049f580417e7c754de888545
Liliia <plv-86@ukr.net>
2025-12-30 17:08:22 +0200

    my first js hw

--- js/task-3.js
+++ js/task-3.js
@@ -1,10 +1,9 @@
 function getElementWidth(content, padding, border) {
-    const contentNum = Number.parseFloat('content');
-    const paddingNum = Number.parseFloat('padding');
-    const borderNum = Number.parseFloat('border');
+    const contentWidth = parseFloat(content);
+    const paddingWidth = parseFloat(padding);
+    const borderWidth = parseFloat(border);
 
-    const elementWidth = contentNum + paddingNum * 2 + borderNum * 2;
-    return elementWidth;
+    return contentWidth + 2 * paddingWidth + 2 * borderWidth;
  };
 console.log(getElementWidth("50px", "8px", "4px")); // 74
 console.log(getElementWidth("60px", "12px", "8.5px")); // 101
